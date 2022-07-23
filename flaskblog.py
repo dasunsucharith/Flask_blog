0df486b2fcb5ed69a631990c663208f26a3c4140
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-07-23 09:32:10 +0530

    new

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for 
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Dasun Sucharith',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'July 23, 2022'
+    },
+    {
+        'author': 'Rebecca Rubasinghe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted', 'July 24, 2022'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
