f24d20d554ef4fb58d2ece2947584f492fe05620
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-08-01 14:41:27 +0530

    new

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '843eaa1491c7a4130988d7c3ee4a9f4c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
+
+
