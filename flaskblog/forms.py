7e59d90f0ca39f70bc44da4a594a04ea9dae4fb5
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-08-02 17:37:18 +0530

    new

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -49,4 +49,6 @@ class UpdateAccountForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    thumbnail = FileField('Add Post Thumbnail', validators=[
+                        FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Post')
