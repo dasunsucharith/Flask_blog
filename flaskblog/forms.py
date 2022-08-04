4b84a044c615bb7b6d277bc77c5db17b4e8422f2
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-08-04 17:35:05 +0530

    new

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -51,3 +51,17 @@ class PostForm(FlaskForm):
     thumbnail = FileField('Add Post Thumbnail', validators=[
                           FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first!')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
