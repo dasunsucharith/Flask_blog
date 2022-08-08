9917adab4959f32e8650ce3fc8bc091026475e97
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-08-08 16:49:46 +0530

    new

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,7 +10,7 @@ from flaskblog.config import Config
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
 
